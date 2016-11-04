eb97aae52366e8d0a97be9d3ae9b9bae6d0c9308
Vadym.Leonov <l1m0n3zon@gmail.com>
2016-11-04 18:21:30 +0200

    m

---
+++ src/index.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Stream from './components/Stream';
+ 
+const tracks = [
+  {
+    title: 'Some track'
+  },
+  {
+    title: 'Some track'
+  }
+];
+ 
+ReactDOM.render(
+  <Stream tracks={tracks} />,
+  document.getElementById('app')
+);
